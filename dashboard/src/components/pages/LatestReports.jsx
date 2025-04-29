42f3287a2f59444a825166bcffee6f0c22e70c19
karekezi fiston <karekezifiston30@gmail.com>
2025-04-29 15:34:59 +0200

    aaa

--- dashboard/src/components/pages/LatestReports.jsx
+++ dashboard/src/components/pages/LatestReports.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import './LatestReports.css'; // Add styles for the component
 
 const LatestReports = () => {
     return (
-        <div>
-            <h2>Latest Reports</h2>
-            {/* Add content for latest reports here */}
+        <div className="latest-reports">
+            <h2>LATESTS REPORTS</h2>
+
+            <div className="report-grid">
+                <div className="grid-item">
+                    <strong>Location</strong>
+                </div>
+                <div className="grid-item">
+                    <strong>Time</strong>
+                </div>
+                <div className="grid-item">
+                    <strong>Report</strong>
+                </div>
+                <div className="grid-item">
+                    <strong>Crime Type</strong>
+                </div>
+            </div>
         </div>
     );
 };
