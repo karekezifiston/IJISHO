91555bab1f17bbcf30fc5f1ec496da8ec4c8ee56
karekezi fiston <karekezifiston30@gmail.com>
2025-06-25 12:47:48 +0200

    aa

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import './navbar.css';
+import './Navbar.css';
 
 const Navbar = () => {
   const { t, i18n } = useTranslation();
